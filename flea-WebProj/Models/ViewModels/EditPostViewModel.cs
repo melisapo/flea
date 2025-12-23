76dc312172c27b6b20171e19e787aca6dd538bf2
melissa <melissagh3009@gmail.com>
2025-12-23 09:17:31 -0500

    created image and editPost view models

--- flea-WebProj/Models/ViewModels/EditPostViewModel.cs
+++ flea-WebProj/Models/ViewModels/EditPostViewModel.cs
@@ -1,9 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using flea_WebProj.Enums;
 
 namespace flea_WebProj.Models.ViewModels;
 
-public class CreatePostViewModel
+public class EditPostViewModel
 {
+    public long PostId { get; set; }
+    public long ProductId { get; set; }
+
     [Required(ErrorMessage = "El título es requerido")]
     [StringLength(255, MinimumLength = 5, ErrorMessage = "El título debe tener entre 5 y 255 caracteres")]
     public string Title { get; set; } = string.Empty;
@@ -16,11 +20,17 @@ public class CreatePostViewModel
     [Range(0.01, 999999.99, ErrorMessage = "El precio debe estar entre 0.01 y 999,999.99")]
     public decimal Price { get; set; }
 
+    [Required(ErrorMessage = "El estado es requerido")]
+    public ProductStatus Status { get; set; }
+
     [Required(ErrorMessage = "Debe seleccionar al menos una categoría")]
     public List<int> CategoryIds { get; set; } = [];
-
-    [Required(ErrorMessage = "Debe subir al menos una imagen")]
-    public List<IFormFile> Images { get; set; } = [];
+    
+    public List<ImageViewModel> ExistingImages { get; set; } = [];
+    
+    public List<IFormFile>? NewImages { get; set; }
+    
+    public List<int> ImagesToDelete { get; set; } = [];
     
     public List<Category> AvailableCategories { get; set; } = [];
 }
---
+++ flea-WebProj/Models/ViewModels/ImageViewModel.cs
@@ -0,0 +1,8 @@
+namespace flea_WebProj.Models.ViewModels;
+
+public class ImageViewModel
+{
+    public long Id { get; set; }
+    public string Path { get; set; } = string.Empty;
+    public string FullUrl { get; set; } = string.Empty;
+}

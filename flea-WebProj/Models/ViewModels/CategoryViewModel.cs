405e0c76d227502e07d69b080adcacacabe08a8d
melissa <melissagh3009@gmail.com>
2025-12-23 09:39:36 -0500

    created ProductDetails and Category view models

---
+++ flea-WebProj/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+namespace flea_WebProj.Models.ViewModels;
+
+public class CategoryViewModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+}
---
+++ flea-WebProj/Models/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,32 @@
+using flea_WebProj.Enums;
+
+namespace flea_WebProj.Models.ViewModels;
+
+public class ProductDetailViewModel
+{
+    public int PostId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    
+    public int ProductId { get; set; }
+    public decimal Price { get; set; }
+    public ProductStatus Status { get; set; }
+    public string StatusText { get; set; } = string.Empty;
+    
+    public List<ImageViewModel> Images { get; set; } = [];
+    
+    public List<CategoryViewModel> Categories { get; set; } = [];
+
+    public int AuthorId { get; set; }
+    public string AuthorUsername { get; set; } = string.Empty;
+    public string AuthorName { get; set; } = string.Empty;
+    public string AuthorProfilePic { get; set; } = string.Empty;
+
+    public string AuthorEmail { get; set; } = string.Empty;
+    public string? AuthorPhoneNumber { get; set; }
+    public string? AuthorTelegramUser { get; set; }
+
+    public bool IsOwner { get; set; } = false;
+}

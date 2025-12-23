95fbba39d20e1e92095ab8cca7c0525da19edfdd
melissa <melissagh3009@gmail.com>
2025-12-22 23:02:56 -0500

    created edit profile and change password view models

---
+++ flea-WebProj/Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace flea_WebProj.Models.ViewModels;
+
+public class ChangePasswordViewModel
+{
+    [Required(ErrorMessage = "La contraseña actual es requerida")]
+    [DataType(DataType.Password)]
+    public string CurrentPassword { get; set; } =  string.Empty;
+    
+    [Required(ErrorMessage = "La nueva contraseña es requerida")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Debe confirmar la nueva contraseña")]
+    [Compare("NewPassword", ErrorMessage = "Las contraseñas no coinciden")]
+    [DataType(DataType.Password)]
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
---
+++ flea-WebProj/Models/ViewModels/EditProfileViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace flea_WebProj.Models.ViewModels;
+
+public class EditProfileViewModel
+{
+    [Required(ErrorMessage = "El nombre completo es requerido")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "El email es requerido")]
+    [EmailAddress(ErrorMessage = "Email inválido")]
+    public string Email { get; set; } = string.Empty;
+    
+    [Phone(ErrorMessage = "Número de teléfono inválido")]
+    public string? PhoneNumber { get; set; }
+    public string? TelegramUser { get; set; }
+    
+    public string? CurrentProfilePic { get; set; }
+    public IFormFile? NewProfilePic { get; set; }
+}

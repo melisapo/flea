2961ee042adbcb72edd2e5a00848c90fef3028e4
melissa <melissagh3009@gmail.com>
2025-12-26 22:03:04 -0500

    added UpdateProfile method to User service

--- flea-WebProj/Services/UserService.cs
+++ flea-WebProj/Services/UserService.cs
@@ -5,6 +5,7 @@ namespace flea_WebProj.Services;
 
 public interface IUserService
 {
+    Task<(bool success, string message)> ChangeUsernameAsync(int userId, ChangeUserViewModel model);
     Task<(bool success, string message)> UpdateProfileAsync(int userId, EditProfileViewModel model);
     Task<(bool success, string message)> ChangePasswordAsync(int userId, ChangePasswordViewModel model);
     Task<(bool success, string message, string? newPath)> UpdateProfilePictureAsync(int userId, IFormFile newProfilePic);
@@ -20,18 +21,46 @@ public class UserService(
     private readonly IContactRepository _contactRepository = contactRepository;
     private readonly IPasswordHasher _passwordHasher = passwordHasher;
     private readonly IFileUploadService _fileUploadService = fileUploadService;
-    
-    public Task<(bool success, string message)> UpdateProfileAsync(int userId, EditProfileViewModel model)
+
+    public Task<(bool success, string message)> ChangeUsernameAsync(int userId, ChangeUserViewModel model)
     {
         throw new NotImplementedException();
     }
 
-    public Task<(bool success, string message)> ChangePasswordAsync(int userId, ChangePasswordViewModel model)
+    public async Task<(bool success, string message)> UpdateProfileAsync(int userId, EditProfileViewModel model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null) 
+                return (false, $"Usuario no encontrado");
+
+            user.Name = model.Name;
+            await _userRepository.UpdateAsync(user);
+        
+            var contact = await _contactRepository.GetByUserIdAsync(userId);
+            if (contact == null) return (false, "Contacto no encontrado");
+            
+            contact.Email = model.Email;
+            contact.PhoneNumber = model.PhoneNumber;
+            contact.TelegramUser = model.TelegramUser;
+            
+            await _contactRepository.UpdateAsync(contact);
+            
+            return (true, "Perfil actualizado exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return (false, $"Error al actualizar perfil: {ex.Message}");
+        }
     }
 
-    public Task<(bool success, string message, string? newPath)> UpdateProfilePictureAsync(int userId, IFormFile newProfilePic)
+    public async Task<(bool success, string message)> ChangePasswordAsync(int userId, ChangePasswordViewModel model)
+    {
+        
+    }
+
+    public async Task<(bool success, string message, string? newPath)> UpdateProfilePictureAsync(int userId, IFormFile newProfilePic)
     {
         throw new NotImplementedException();
     }

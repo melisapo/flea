26ef23068f1acb8671caceca1c51ce9365c20e2a
melissa <melissagh3009@gmail.com>
2026-01-02 11:42:56 -0500

    added admin dashboard view model

---
+++ flea-WebProj/Models/ViewModels/Admin/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+using flea_WebProj.Models.Entities;
+
+namespace flea_WebProj.Models.ViewModels.Admin;
+
+public class DashboardViewModel
+{
+    public int TotalUsers { get; set; }
+    public int TotalPosts { get; set; }
+    public List<Post>? PostsToday { get; set; }
+    public int TotalCategories { get; set; }
+    public List<Category>? TrendingCategories { get; set; }
+}
